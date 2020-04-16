d3c9d2ea60064c4a44afbdfc8e4f1c88e9d75667
Jimmie Max <jimmiemaxgo@gmail.com>
2020-04-16 11:59:08 +0800

    feat[electron]add

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,21 @@
 module.exports = {
-    devServer: {
-      proxy: {
-        "/v1": {
-            "target": "http://localhost:5500",
-            "changeOrigin": true
-          }
+  baseUrl: './',
+  devServer: {
+    proxy: {
+      "/v1": {
+        "target": "http://api.geek.cn",
+        "changeOrigin": true
       }
-    },
-    css: {
-      loaderOptions: {
-        // 给 sass-loader 传递选项
-        sass: {
-          // @/ 是 src/ 的别名
-          // 所以这里假设你有 `src/variables.scss` 这个文件
-          data: `@import "@/assets/style/variables.scss";`
-        }
+    }
+  },
+  css: {
+    loaderOptions: {
+      // 给 sass-loader 传递选项
+      sass: {
+        // @/ 是 src/ 的别名
+        // 所以这里假设你有 `src/variables.scss` 这个文件
+        data: `@import "@/assets/style/variables.scss";`
       }
     }
   }
+}
