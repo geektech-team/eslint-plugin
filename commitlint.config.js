7df97f1f2c40becf555ba03b625c5e6ae8e31ecf
Jimmie Max <jimmiemaxgo@gmail.com>
2021-03-23 17:18:01 +0800

    s

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
+        "header-max-length": () => [0, "always", 2],
         'type-enum': [
             2,
             'always',
